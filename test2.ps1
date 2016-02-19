3a4d1469b35711dbb79b2e63dbc2ae84463fc74c
Marcos Santos <meliehl@outlook.com>
2016-02-18 22:44:23 -0200

    test with another scripts

---
+++ test2.ps1
@@ -0,0 +1,19 @@
+$PSScriptFilePath = (Get-Item $MyInvocation.MyCommand.Path).FullName
+$SolutionRoot = Split-Path -Path $PSScriptFilePath -Parent
+
+" PSScriptFilePath = $PSScriptFilePath"
+$DNU = "dnu"
+$DNX = "dnx"
+$DNVM = "dnvm"
+
+# ensure the correct version
+& $DNVM install 1.0.0-rc1-final -r coreclr
+
+# use the correct version
+& $DNVM use 1.0.0-rc1-final -r coreclr
+
+Get-ChildItem -Path $PSScriptRoot\test -Filter project.json -Depth 1 | ForEach-Object { 
+	& $DNU restore $_.FullName 2>1 
+	& $DNU build $_.FullName 2>1 
+	& $DNX -p $_.FullName 2>1 test
+}
