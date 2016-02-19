6a1274e7101a6ebdf34ea705e84a9e0f3836f9e5
Marcos Santos <meliehl@outlook.com>
2016-02-18 22:59:33 -0200

    Build with trow

--- test2.ps1
+++ test2.ps1
@@ -12,14 +12,26 @@ $DNVM = "dnvm"
 # use the correct version
 & $DNVM use 1.0.0-rc1-final -r coreclr
 
-Get-ChildItem -Path $PSScriptRoot\test -Filter project.json -Depth 1 | ForEach-Object { 
+Get-ChildItem -Path $PSScriptRoot\test -Filter project.json -Recurse | ForEach-Object { 
 	& $DNU restore $_.FullName 2>1
+	if (-not $?)
+	{
+		throw "The DNU restore process returned an error code."
+	}
 }
 
-Get-ChildItem -Path $PSScriptRoot\test -Filter project.json -Depth 1 | ForEach-Object { 
+Get-ChildItem -Path $PSScriptRoot\test -Filter project.json -Recurse | ForEach-Object { 
 	& $DNU build $_.FullName 2>1 	
+	if (-not $?)
+	{
+		throw "The DNU build process returned an error code."
+	}
 }
 
-Get-ChildItem -Path $PSScriptRoot\test -Filter project.json -Depth 1 | ForEach-Object { 
+Get-ChildItem -Path $PSScriptRoot\test -Filter project.json -Recurse | ForEach-Object { 
 	& $DNX -p $_.FullName 2>1 test
+	if (-not $?)
+	{
+		throw "The dnx test process returned an error code."
+	}
 }
