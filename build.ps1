429513ec8cc305d3bdc0036534d9c1bcb4351351
Marcos Santos <meliehl@outlook.com>
2016-02-22 19:05:03 -0300

    dnu pack

--- build.ps1
+++ build.ps1
@@ -1,6 +1,21 @@
+param (
+	[Parameter(Mandatory=$true)]
+	[ValidatePattern("^\d+\.\d+\.(?:\d+\.\d+$|\d+$)")]
+	[string]
+	$ReleaseVersionNumber,
+	[Parameter(Mandatory=$true)]
+	[string]
+	[AllowEmptyString()]
+	$PreReleaseName
+)
+
 $PSScriptFilePath = (Get-Item $MyInvocation.MyCommand.Path).FullName
 $SolutionRoot = Split-Path -Path $PSScriptFilePath -Parent
 $ProjectsPath = Join-Path -Path $SolutionRoot -ChildPath "src"
+
+# Make sure we don't have a release folder for this version already
+$BuildFolder = Join-Path -Path $SolutionRoot -ChildPath "build";
+$ReleaseFolder = Join-Path -Path $BuildFolder -ChildPath "Releases\v$ReleaseVersionNumber$PreReleaseName";
 
 " PSScriptFilePath = $PSScriptFilePath"
 $DNU = "dnu"
@@ -25,4 +40,18 @@ if (-not $?)
 {
 	throw "The DNU build process returned an error code."
 }
-cd $SolutionRoot
+
+
+if ((Get-Item $ReleaseFolder -ErrorAction SilentlyContinue) -ne $null)
+{
+	Write-Warning "$ReleaseFolder already exists on your local machine. It will now be deleted."
+	Remove-Item $ReleaseFolder -Recurse
+}
+
+& $DNU pack "\*" --configuration Release --out "$ReleaseFolder"
+if (-not $?)
+{
+	throw "The DNU pack process returned an error code."
+}
+
+cd "$SolutionRoot"
